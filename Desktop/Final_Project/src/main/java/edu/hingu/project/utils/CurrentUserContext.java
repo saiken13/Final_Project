d0056ccb2dd6264061cbe359850dadb3caf98ee3
Kapil Hingu <38289510+saiken13@users.noreply.github.com>
2025-06-04 15:17:07 -0500

    Delete Desktop/Final_Project directory

--- Desktop/Final_Project/src/main/java/edu/hingu/project/utils/CurrentUserContext.java
+++
@@ -1,7 +0,0 @@
-package edu.hingu.project.utils;
-
-import org.springframework.security.core.Authentication;
-
-import edu.hingu.project.entities.User;
-
-public record CurrentUserContext(User user, Authentication auth) {}
